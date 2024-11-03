873697e64ea9f2823f5c6ee8fd1b1eba79ea4ab2
thaianh26922 <thaianh26902@gmail.com>
2024-11-03 16:26:04 +0700

    Update deploy

--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,16 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 8686,
     strictPort: true,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'ssl-certs/private-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'ssl-certs/certificate.crt'))
-    },
-    watch: {
-      usePolling: true,
-    },
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, 'ssl-certs/private-key.pem')),
+    //   cert: fs.readFileSync(path.resolve(__dirname, 'ssl-certs/certificate.crt'))
+    // },
+    // watch: {
+    //   usePolling: true,
+    // },
     proxy: {
-      '/api': {
-        target: 'http://192.168.1.4:5001',
+      '^/api': {
+        target: 'https://localhost:4443/api',
         changeOrigin: true,
         secure: true, // Chỉ dùng trong môi trường phát triển
       }
